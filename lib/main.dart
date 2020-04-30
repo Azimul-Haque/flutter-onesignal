a143ba4700dc554894e0178483632ed5c9806dd9
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-30 22:42:21 +0600

    ASDASD

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  title: 'BCS Charioteer',
+));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
