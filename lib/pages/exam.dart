7f846494f478b4b3eccf2e6e67ba00ea23bad894
orbachinujbuk <orbachinujbuk@gmail.com>
2020-05-19 18:11:44 +0600

    ASDa34r

--- lib/pages/exam.dart
+++ lib/pages/exam.dart
@@ -9,24 +9,70 @@ class ExamPage extends StatefulWidget {
 class _ExamPageState extends State<ExamPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ইতিহাস"),
-        automaticallyImplyLeading: false,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.check), 
-            onPressed: () async{
-              Navigator.pop(context);
-            },
-            tooltip: "দাখিল করুন",
-          ),
-        ],
-        flexibleSpace: appBarStyle(),
+    return WillPopScope(
+      onWillPop: () {
+        showAlertDialog();
+        return null;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("ইতিহাস"),
+          automaticallyImplyLeading: false,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.check), 
+              onPressed: () async{
+                Navigator.pop(context);
+              },
+              tooltip: "দাখিল করুন",
+            ),
+          ],
+          flexibleSpace: appBarStyle(),
+        ),
+        body: ListView(children: <Widget>[
+          Text("Exam Page"),
+        ],),
       ),
-      body: ListView(children: <Widget>[
-        Text("Exam Page"),
-      ],),
+    );
+  }
+
+  showAlertDialog() {
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Center(child: Text('পরীক্ষা শেষ করবেন?')),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children : <Widget>[
+          CircularProgressIndicator(),
+        ],
+      ),
+      actions: <Widget>[
+        RaisedButton(
+          child: Text("শেষ করুন"),
+          color: Colors.green,
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.pop(context);
+          },
+        ),
+        RaisedButton(
+          child: Text("পরীক্ষা দিতে থাকুন"),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
