c24752103b0cf6d9b7ba3b771dcce454ce97fa2b
orbachinujbuk <orbachinujbuk@gmail.com>
2020-05-19 17:59:03 +0600

    WAS324

--- lib/pages/exam.dart
+++ lib/pages/exam.dart
@@ -10,7 +10,23 @@ class _ExamPageState extends State<ExamPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("ইতিহাস"),
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check), 
+            onPressed: () async{
+              Navigator.pop(context);
+            },
+            tooltip: "দাখিল করুন",
+          ),
+        ],
+        flexibleSpace: appBarStyle(),
+      ),
+      body: ListView(children: <Widget>[
+        Text("Exam Page"),
+      ],),
     );
   }
 }
