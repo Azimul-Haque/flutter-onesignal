752538b3bdc2c180919be8566b46ac92f50b9edf
orbachinujbuk <orbachinujbuk@gmail.com>
2020-05-30 01:00:13 +0600

    ASD234

--- android/app/src/main/kotlin/com/example/project1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project1/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
