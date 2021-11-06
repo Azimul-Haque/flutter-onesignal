9683a99f24d13f9f87fc391bb25171ce70a8eedd
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2021-11-07 01:23:10 +0600

    ASD34

--- lib/pages/constitution/part11.dart
+++ lib/pages/constitution/part11.dart
@@ -10,7 +10,7 @@ class PartEleven extends StatelessWidget {
         // automaticallyImplyLeading: false,
         // actions: <Widget>[
         //   IconButton(
-        //     icon: Icon(Icons.check), 
+        //     icon: Icon(Icons.check),
         //     onPressed: () async{
         //       Navigator.pop(context);
         //     },
@@ -19,23 +19,36 @@ class PartEleven extends StatelessWidget {
         // ],
         flexibleSpace: appBarStyle(),
       ),
-      body: ListView(children: <Widget>[
-        SizedBox(height: 10,),
-        Text("(একাদশ ভাগ) বিবিধ", style: TextStyle(color:Colors.green[800], fontSize: 18), textAlign: TextAlign.center,),
-        SizedBox(height: 3,),
-        Divider(),
-        Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(children: <Widget>[
-            Text(
-              "১৪৩। প্রজাতন্ত্রের সম্পত্তি \n\n১৪৪। সম্পত্তি ও কারবার প্রভৃতি-প্রসঙ্গে নির্বাহী কর্তৃত্ব \n\n১৪৫। চুক্তি ও দলিল \n\n১৪৫ক। আন্তর্জাতিক চুক্তি \n\n১৪৬। বাংলাদেশের নামে মামলা \n\n১৪৭। কতিপয় পদাধিকারীর পারিশ্রমিক প্রভৃতি \n\n১৪৮। পদের শপথ \n\n১৪৯। প্রচলিত আইনের হেফাজত \n\n১৫০। ক্রান্তিকালীন ও অস্থায়ী বিধানাবলী \n\n১৫১। রহিতকরণ \n\n১৫২। ব্যাখ্যা \n\n১৫৩। প্রবর্তন, উল্লেখ ও নির্ভরযোগ্য পাঠ", 
-              style: TextStyle(color:Colors.black87, fontSize: 15, height: 1.5), textAlign: TextAlign.justify,
-            ),
-            SizedBox(height: 10,),
-
-          ]),
-        ),
-      ],),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "(একাদশ ভাগ) বিবিধ",
+            style: TextStyle(color: Colors.green[800], fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Divider(),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(children: <Widget>[
+              Text(
+                "১৪৩। প্রজাতন্ত্রের সম্পত্তি \n\n১৪৪। সম্পত্তি ও কারবার প্রভৃতি-প্রসঙ্গে নির্বাহী কর্তৃত্ব \n\n১৪৫। চুক্তি ও দলিল \n\n১৪৫ক। আন্তর্জাতিক চুক্তি \n\n১৪৬। বাংলাদেশের নামে মামলা \n\n১৪৭। কতিপয় পদাধিকারীর পারিশ্রমিক প্রভৃতি \n\n১৪৮। পদের শপথ \n\n১৪৯। প্রচলিত আইনের হেফাজত \n\n১৫০। ক্রান্তিকালীন ও অস্থায়ী বিধানাবলী \n\n১৫১। রহিতকরণ \n\n১৫২। ব্যাখ্যা \n\n১৫৩। প্রবর্তন, উল্লেখ ও নির্ভরযোগ্য পাঠ",
+                style:
+                    TextStyle(color: Colors.black87, fontSize: 15, height: 1.5),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/constitution/part7.dart
+++ lib/pages/constitution/part7.dart
@@ -10,7 +10,7 @@ class PartSeven extends StatelessWidget {
         // automaticallyImplyLeading: false,
         // actions: <Widget>[
         //   IconButton(
-        //     icon: Icon(Icons.check), 
+        //     icon: Icon(Icons.check),
         //     onPressed: () async{
         //       Navigator.pop(context);
         //     },
@@ -19,23 +19,36 @@ class PartSeven extends StatelessWidget {
         // ],
         flexibleSpace: appBarStyle(),
       ),
-      body: ListView(children: <Widget>[
-        SizedBox(height: 10,),
-        Text("(সপ্তম ভাগ) নির্বাচন", style: TextStyle(color:Colors.green[800], fontSize: 18), textAlign: TextAlign.center,),
-        SizedBox(height: 3,),
-        Divider(),
-        Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(children: <Widget>[
-            Text(
-              "১১৮। নির্বাচন কমিশন প্রতিষ্ঠা \n\n১১৯। নির্বাচন কমিশনের দায়িত্ব \n\n১২০। নির্বাচন কমিশনের কর্মচারীগণ \n\n১২১। প্রতি এলাকার জন্য একটিমাত্র ভোটার তালিকা \n\n১২২। ভোটার-তালিকায় নামভুক্তির যোগ্যতা \n\n১২৩। নির্বাচন-অনুষ্ঠানের সময় \n\n১২৪। নির্বাচন সম্পর্কে সংসদের বিধান প্রণয়নের ক্ষমতা \n\n১২৫। নির্বাচনী আইন ও নির্বাচনের বৈধতা \n\n১২৬। নির্বাচন কমিশনকে নির্বাহী কর্তৃপক্ষের সহায়তাদান", 
-              style: TextStyle(color:Colors.black87, fontSize: 15, height: 1.5), textAlign: TextAlign.justify,
-            ),
-            SizedBox(height: 10,),
-
-          ]),
-        ),
-      ],),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "(সপ্তম ভাগ) নির্বাচন",
+            style: TextStyle(color: Colors.green[800], fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Divider(),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(children: <Widget>[
+              Text(
+                "১১৮। নির্বাচন কমিশন প্রতিষ্ঠা \n\n১১৯। নির্বাচন কমিশনের দায়িত্ব \n\n১২০। নির্বাচন কমিশনের কর্মচারীগণ \n\n১২১। প্রতি এলাকার জন্য একটিমাত্র ভোটার তালিকা \n\n১২২। ভোটার-তালিকায় নামভুক্তির যোগ্যতা \n\n১২৩। নির্বাচন-অনুষ্ঠানের সময় \n\n১২৪। নির্বাচন সম্পর্কে সংসদের বিধান প্রণয়নের ক্ষমতা \n\n১২৫। নির্বাচনী আইন ও নির্বাচনের বৈধতা \n\n১২৬। নির্বাচন কমিশনকে নির্বাহী কর্তৃপক্ষের সহায়তাদান",
+                style:
+                    TextStyle(color: Colors.black87, fontSize: 15, height: 1.5),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
