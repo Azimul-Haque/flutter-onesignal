e351ff6dfbab37d668fac6513cbed696e2fd3793
Azimul-Haque <orbachinujbuk@gmail.com>
2022-03-27 23:50:12 +0600

    Commit Sun 03/27/2022 23:50:12.17 2361

--- lib/pages/constitution/part10.dart
+++ lib/pages/constitution/part10.dart
@@ -33,11 +33,29 @@ class PartTen extends StatelessWidget {
             height: 3,
           ),
           Divider(),
+          SizedBox(
+            height: 3,
+          ),
+          ExpansionTile(
+            title: Text('১৪২। সংবিধানের বিধান সংশোধনের ক্ষমতা'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                '৮৷ (১) জাতীয়তাবাদ, সমাজতন্ত্র, গণতন্ত্র ও ধর্মনিরপেক্ষতা- এই নীতিসমূহ এবং তৎসহ এই নীতিসমূহ হইতে উদ্ভূত এই ভাগে বর্ণিত অন্য সকল নীতি রাষ্ট্র পরিচালনার মূলনীতি বলিয়া পরিগণিত হইবে।',
+                textAlign: TextAlign.justify,
+              )),
+              ListTile(
+                  title: Text(
+                '(২) এই ভাগে বর্ণিত নীতিসমূহ বাংলাদেশ-পরিচালনার মূলসূত্র হইবে, আইন-প্রণয়নকালে রাষ্ট্র তাহা প্রয়োগ করিবেন, এই সংবিধান ও বাংলাদেশের অন্যান্য আইনের ব্যাখ্যাদানের ক্ষেত্রে তাহা নির্দেশক হইবে এবং তাহা রাষ্ট্র ও নাগরিকদের কার্যের ভিত্তি হইবে, তবে এই সকল নীতি আদালতের মাধ্যমে বলবৎযোগ্য হইবে না৷',
+                textAlign: TextAlign.justify,
+              ))
+            ],
+          ),
           Padding(
             padding: EdgeInsets.all(15),
             child: Column(children: <Widget>[
               Text(
-                "১৪২। সংবিধানের বিধান সংশোধনের ক্ষমতা",
+                "",
                 style:
                     TextStyle(color: Colors.black87, fontSize: 15, height: 1.5),
                 textAlign: TextAlign.justify,
