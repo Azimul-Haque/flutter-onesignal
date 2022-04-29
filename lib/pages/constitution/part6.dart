11ee8ea21d6df02d3f6fa4204f5d57481a47f307
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-29 14:32:06 +0600

    Commit Fri 04/29/2022 14:32:06.25 21495

--- lib/pages/constitution/part6.dart
+++ lib/pages/constitution/part6.dart
@@ -416,20 +416,6 @@ class PartSix extends StatelessWidget {
               )),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: Column(children: <Widget>[
-              Text(
-                "\n\n\n \n\n \n\n \n\n \n\n \n\n\n \n\n \n\n\nষষ্ঠ ক ভাগ-জাতীয়দল [সংবিধান (পঞ্চদশ সংশোধন) আইন, ২০১১ (২০১১ সনের ১৪ নং আইন)-এর ৪১ ধারাবলে বিলুপ্ত।]",
-                style:
-                    TextStyle(color: Colors.black87, fontSize: 15, height: 1.5),
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-            ]),
-          ),
         ],
       ),
     );
