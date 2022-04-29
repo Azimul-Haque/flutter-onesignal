2a33fd063e2b286b544e0a659e3d31555268134b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 02:55:06 +0600

    Commit Sat 04/30/2022  2:55:06.19 16053

--- lib/main.dart
+++ lib/main.dart
@@ -803,10 +803,8 @@ class _HomePageState extends State<HomePage> {
               height: 30,
             ),
             Text('একটি Innova Tech Inc. পণ্য'),
-            Text(
-              "Version: " + _packageInfo.version,
-              style: TextStyle(color: Colors.black54, fontSize: 13),
-            )
+            Text("Version: " + _packageInfo.version,
+                style: TextStyle(color: Colors.black54, fontSize: 13))
           ],
         ),
       ),
