653d98ddfbcfe3dac3d91f8393e281efff6a87bd
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 02:38:54 +0600

    Commit Sat 04/30/2022  2:38:54.19 14056

--- lib/main.dart
+++ lib/main.dart
@@ -749,8 +749,8 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             Image.asset(
               "assets/images/android-icon.png",
-              width: 60,
-              height: 60,
+              width: 50,
+              height: 50,
             ),
             SizedBox(
               height: 10,
