112edb1f0a259b1262944c433f0fabf1553deae0
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 01:31:16 +0600

    Commit Sat 04/30/2022  1:31:16.27 32487

--- lib/pages/constitution/part9.dart
+++ lib/pages/constitution/part9.dart
@@ -259,20 +259,6 @@ class PartNine extends StatelessWidget {
               )),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: Column(children: <Widget>[
-              Text(
-                " \n\n \n\n",
-                style:
-                    TextStyle(color: Colors.black87, fontSize: 15, height: 1.5),
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-            ]),
-          ),
         ],
       ),
     );
