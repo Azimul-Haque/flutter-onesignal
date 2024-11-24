9acb96212c6a77806b2b0ef2b6ca213552749f53
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 01:50:42 +0600

    Commit Mon 11/25/2024  1:50:42.22 32610

--- lib/pages/exam.dart
+++ lib/pages/exam.dart
@@ -313,6 +313,9 @@ class _ExamPageState extends State<ExamPage> {
       ),
       actions: <Widget>[
         ElevatedButton(
+          style: ButtonStyle(
+            textStyle: TextStyle(color: Colors.amber),
+          ),
           child: Text("শেষ করুন"),
           style: ButtonStyle(
             backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
